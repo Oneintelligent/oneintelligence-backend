35a85b224b4921f37a2fd01900450bd9a3743c81
Jaga Nachimuthu <oneintelligencetech@gmail.com>
2025-11-09 14:08:23 +0530

    local url

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-placeholder-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.9', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
